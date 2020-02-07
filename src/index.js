efc9f6aeed3e23520ed2351cf86c522d0ee450e4
Adam <adam.biagianti@gmail.com>
2020-02-06 17:26:09 -0800

    Remove magenta and put it in a branch where it belongs

--- src/index.js
+++ src/index.js
@@ -3,36 +3,7 @@ import sequencer from 'services/sequencer';
 import buildScore from 'services/score-builder';
 import recorder from 'services/recorder';
 import { bootstrap, tock, static18f } from 'data/repos';
-import melodyModel from 'services/melody-generator';
 
-const NOTES = {
-  notes: [
-    {pitch: 60, startTime: 0.0, endTime: 0.5},
-    {pitch: 60, startTime: 0.5, endTime: 1.0},
-    {pitch: 67, startTime: 1.0, endTime: 1.5},
-    {pitch: 67, startTime: 1.5, endTime: 2.0},
-    {pitch: 69, startTime: 2.0, endTime: 2.5},
-    {pitch: 69, startTime: 2.5, endTime: 3.0},
-    {pitch: 67, startTime: 3.0, endTime: 4.0},
-    {pitch: 65, startTime: 4.0, endTime: 4.5},
-    {pitch: 65, startTime: 4.5, endTime: 5.0},
-    {pitch: 64, startTime: 5.0, endTime: 5.5},
-    {pitch: 64, startTime: 5.5, endTime: 6.0},
-    {pitch: 62, startTime: 6.0, endTime: 6.5},
-    {pitch: 62, startTime: 6.5, endTime: 7.0},
-    {pitch: 60, startTime: 7.0, endTime: 8.0},  
-  ],
-  totalTime: 8
-};
-
-melodyModel()
-  .then((magenta) => {
-    const qns = magenta.sequences.quantizeNoteSequence(NOTES, 4);
-    magenta.model.continueSequence(qns, 20, 2.0)
-      .then((sample) => {
-        console.log(magenta.sequences.unquantizeSequence(sample))
-      });
-  });
 
 const testRepos = {
   tock: {
