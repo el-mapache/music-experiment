2aa8a1fe9586dca0c09212ca1eb138597428d559
Adam Biagianti <adam.biagianti@gmail.com>
2020-02-08 15:57:00 -0800

    Update chord model with documentation, math.max

--- src/services/chord.js
+++ src/services/chord.js
@@ -1,7 +1,3 @@
-// now not sure about this abstraction. do i also need a note?
-// probably, that could be the composed series of functions that
-// gets the octave, note name and any modifiers like sharps/flats/accents,
-// or ties?
 /**
  * Returns a note in the supplied scale and the note's octave
  * as a string
@@ -18,13 +14,35 @@ const getNoteAndOctave = (scale, octaveMin, octaveMax, notePosition) => {
   return `${scale[notePosition]}${''}${octave}`;
 };
 
+/**
+ * Represents an abstract container for a group of sounds.
+ * It's purpose is to provide general information about the sounds are
+ * arrayed at a particular point it time:
+ * - their overall volume
+ * - the scale from which the sounds are derived, and the resultant note names
+ * - the speed of the slowest note. This is particularly important,
+ * as it determines how long this cluster of tones will sound.
+ * 
+ * This class DOES NOT contain any information about the quality of the notes
+ * it contains, beyond the raw note name. Actual note frequency and timing information
+ * lives in the Note class
+ */
 class Chord {
+  /**
+   * @param {Object} props The properties this class accepts
+   * @param {Number} props.volume The overall volume of the group of notes this object describes
+   * @param {Array} props.scale The musical scale this group of tones pulls from, represented as a 7-element list
+   * @returns Chord
+   */
   constructor({ volume, scale }) {
+    // Speed helps determine the length of the note. 0 -> slowest, 100 -> fastest
     this.speed = 0;
     // maybe i can randomly assign these volumes so
     // like, a single note can randomly have either a higher volume
     // or not, basically an accent?
     this.volume = volume;
+    // Array of strings representing the note name in western English standard musical notation
+    // @example E4
     this.notes = [];
     this.scale = scale;
   }
@@ -39,9 +57,7 @@ class Chord {
         );
       }
 
-      if (el > this.speed) {
-        this.speed = el;
-      }
+      this.speed = Math.max(el, this.speed);
 
       return notes;
     }, [0]);
